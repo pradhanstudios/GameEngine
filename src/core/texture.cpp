47cdf8e498bf10297e568d54cccbe1959cb12f3e
rp <pradhanr@mailfence.com>
2025-04-27 15:52:28 -0700

    Added loading BPMs to textures

---
+++ src/core/texture.cpp
@@ -0,0 +1,43 @@
+#include "texture.hpp"
+
+GLuint loadBMPTexture(const char* filename) {
+	FILE* file = fopen(filename, "rb");
+	unsinged char *imageData;
+	unsigned char header[54];
+	unsigned int height;
+	unsigned int width;
+	unsigned int imageSize; // width * height * 3 because there are three color channels
+	if (fread(header, 1, 54) != 54) { // not 54 bytes
+		printf("Not a BMP file or formatted incorrectly");
+		return;
+	}
+
+	// check if it is actually a BMP file
+	if (header[0] != 'B' || header[1] != 'M') {
+		printf("Not a BMP file or formatted incorrectly")
+		return;
+	}
+
+	// Collect information from the headers
+	// About *( int* ) &, we are converting the value into a char *, and then converting it to int *, and then getting the value
+	width = *( int* ) &(header[0x12]);	
+	height = *( int* ) &(header[0x16]);
+	imageSize = width * height * 3;
+
+	// Allocate memory for the image data
+	imageData = new unsigned char[imageSize];
+	// Read the data
+	fread(imageData, 1, imageSize, file);
+	fclose(file);
+	GLuint textureID;
+	// Generate Texture ID
+	glGenTextures(1, &textureID);
+	// Bind the Texture ID
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	// Give the image data to openGL and link it to the Texture ID 
+	GLTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, imageData);
+
+	delete[] imageData;
+
+	return textureID;
+}
---
+++ src/core/texture.hpp
@@ -0,0 +1,8 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include "constants.hpp"
+
+GLuint loadBMPTexture(const char* filename);
+
+#endif // TEXTURE_HPP
