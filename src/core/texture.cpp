6eaf6517d518f6f86259cbac6c5fa7a191829161
rp <pradhanr@mailfence.com>
2025-04-28 17:35:36 -0700

    Fixed some issues with load_bmp function

--- src/core/texture.cpp
+++ src/core/texture.cpp
@@ -2,20 +2,20 @@
 
 GLuint loadBMPTexture(const char* filename) {
 	FILE* file = fopen(filename, "rb");
-	unsinged char *imageData;
+	unsigned char *imageData;
 	unsigned char header[54];
 	unsigned int height;
 	unsigned int width;
 	unsigned int imageSize; // width * height * 3 because there are three color channels
-	if (fread(header, 1, 54) != 54) { // not 54 bytes
+	if (fread(header, 1, 54, file) != 54) { // not 54 bytes
 		printf("Not a BMP file or formatted incorrectly");
-		return;
+		return 0;
 	}
 
 	// check if it is actually a BMP file
 	if (header[0] != 'B' || header[1] != 'M') {
-		printf("Not a BMP file or formatted incorrectly")
-		return;
+		printf("Not a BMP file or formatted incorrectly");
+		return 0;
 	}
 
 	// Collect information from the headers
@@ -35,7 +35,7 @@ GLuint loadBMPTexture(const char* filename) {
 	// Bind the Texture ID
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	// Give the image data to openGL and link it to the Texture ID 
-	GLTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, imageData);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, imageData);
 
 	delete[] imageData;
 
