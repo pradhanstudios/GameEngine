81b11ea3daecd29d8865bb872552cbee242bbb74
rp <pradhanr@mailfence.com>
2025-05-03 17:47:16 -0700

    Added glutSwapBuffers

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@ ObjectList objects;
 void init() { 
     // background color 
     glClearColor(0.0, 0.0, 0.0, 1.0); 
+    glClearStencil(0);
       
-    // making picture color green 
+    // making picture color white 
     glColor3f(1.0, 1.0, 1.0);
     
     glEnable(GL_TEXTURE_2D);
@@ -24,7 +25,7 @@ void init() {
 } 
   
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT); 
+    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); 
     for (int i = 0; i < objects.size(); i++) {
         if (Collision* collision = dynamic_cast<Collision*>(objects[i].get())) {
             for (int j = 0; j < objects.size(); j++) {
@@ -43,12 +44,13 @@ void display() {
 
     glFlush();
     glutPostRedisplay();
+    glutSwapBuffers();
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
 } 
   
 int main(int argc, char** argv) { 
     glutInit(&argc, argv); 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_STENCIL | GLUT_DEPTH); 
       
     // giving window size in X- and Y- directon 
     glutInitWindowSize(RESOLUTION_WIDTH, RESOLUTION_HEIGHT); 
@@ -64,7 +66,7 @@ int main(int argc, char** argv) {
     printf("Address of rickroll: %p\n", &rickroll);
     objects.push_back(std::make_unique<MyCircle>(MyCircle(RESOLUTION_WIDTH / 3, 0, 50, &rickroll)));
     objects[0]->setCenterY(RESOLUTION_HEIGHT / 2);
-    objects.push_back(std::make_unique<Rectangle>(Rectangle(RESOLUTION_WIDTH / 3 * 2, 0, 200, 100)));
+    objects.push_back(std::make_unique<Rectangle>(Rectangle(RESOLUTION_WIDTH / 3 * 2, 0, 200, 100, &rickroll)));
     objects[1]->setCenterY(RESOLUTION_HEIGHT / 2);
     objects.push_back(std::make_unique<Rectangle>(Rectangle(0, RESOLUTION_HEIGHT - 50, RESOLUTION_WIDTH, 50)));
     objects[2]->setCenterX(RESOLUTION_WIDTH / 2);
