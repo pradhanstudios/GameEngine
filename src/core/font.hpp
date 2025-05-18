a27ca9e94206bce012c55ace012a99e5ac2a9372
rp <pradhanr@mailfence.com>
2025-05-17 17:06:30 -0700

    Added Character class

--- src/core/font.hpp
+++ src/core/font.hpp
@@ -5,28 +5,35 @@
 #include "constants.hpp"
 #include "vector.hpp"
 #include "texture.hpp"
+#include "shape.hpp"
+
+class Character : public Texture {
+public:
+    int bearingX, bearingY, advance;
+    Character(GLuint textureID=0, int width=0, int height=0, int bearingX=0, int bearingY=0, int advance=0) : Texture(textureID, width, height), bearingX(bearingX), bearingY(bearingY), advance(advance) {}   
+};
 
 struct Font {
 private:
-    Texture characters[95];
+    Character characters[95];
 public:
     Font(const char* path);
     void renderSentence(const char* sentence, int fontSize, Vector position, GLuint shader);
 
-    inline Vector getCharacterSize(Texture character, int fontSize=48) {
+    inline Vector getCharacterSize(Character character, int fontSize=48) {
         float scale = fontSize / 48.f;
         return Vector(character.width * scale, character.height * scale);
     }
 
-    inline void setCharacter(char character, Texture texture) {
+    inline void setCharacter(char character, Character texture) {
         characters[character - 32] = texture;
     }
 
-    inline Texture getCharacter(char character) {
+    inline Character getCharacter(char character) {
         return characters[character - 32];
     }
 };
 
-inline Font roboto = Font("assets/Roboto.ttf");
+inline Font* roboto;
 
 #endif // FONT_HPP
