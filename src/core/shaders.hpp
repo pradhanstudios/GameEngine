d4d0f7c8091f236d93efe597080753750bd12547
rp <pradhanr@mailfence.com>
2025-05-17 22:52:45 -0700

    Add global imageShader variable

--- src/core/shaders.hpp
+++ src/core/shaders.hpp
@@ -6,5 +6,6 @@ char* readFile(const char* filename);
 GLuint initShaders(const char* vertexShader, const char* fragShader);
 
 extern GLuint textShader;
+extern GLuint imageShader;
 
 #endif // SHADER_HPP
