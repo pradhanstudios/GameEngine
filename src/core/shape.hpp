f8a509f5fc6acbca249fc394673ed9d8ec09162d
rp <pradhanr@mailfence.com>
2025-06-25 09:49:20 -0700

    Changed order of many function parameters

--- src/core/shape.hpp
+++ src/core/shape.hpp
@@ -6,9 +6,9 @@
 #include "object.hpp"
 #include "vector.hpp"
 
-void _drawShape(const glm::mat4& model, const glm::mat4& view, const glm::mat4& projection, GLuint VAO, GLsizei vertexCount, GLuint shader, GLenum primitiveType, GLenum textureUnit, bool useElements, GLuint textureID, Vector3 color, bool useColor);
-void drawRectangleManual(Vector position, Texture* texture, int width, int height, GLuint shader, Vector3 color=WHITE, bool useColor=NO_USE_COLOR, float rotationAngleRad=0.f);
-void drawCircleManual(Vector position, Texture* texture, int radius, float rotation, GLuint shader, Vector3 color=WHITE, bool useColor=NO_USE_COLOR);
+void _drawShape(const glm::mat4& model, const glm::mat4& view, const glm::mat4& projection, GLuint VAO, GLsizei vertexCount, GLuint shader, GLenum primitiveType, GLenum textureUnit, bool useElements, GLuint textureID, vec3 color, bool useColor);
+void drawRectangleManual(vec2 centerPosition, float width, float height, float rotation, Texture* texture, GLuint shader, vec3 color=WHITE, bool useColor=NO_USE_COLOR);
+void drawCircleManual(vec2 position, float radius, float rotation, Texture* texture, GLuint shader, vec3 color=WHITE, bool useColor=NO_USE_COLOR);
 
 struct Projection {
     float min, max;
@@ -17,21 +17,21 @@ struct Projection {
 class Circle : public Object {
 public:
     float radius;
-    Circle(int x, int y, float radius, Texture* texture=nullptr, float rotation=0.f) :
-        Object(x, y, radius * 2, radius * 2, texture, rotation), radius(radius) {}
+    Circle(int x, int y, float radius, float rotation=0.f, Texture* texture=nullptr) :
+        Object(x, y, radius * 2, radius * 2, rotation, texture), radius(radius) {}
 
-    Circle(Vector position, float radius, Texture* texture=nullptr, float rotation=0.f) :
-        Object(position, radius * 2, radius * 2, texture, rotation), radius(radius) {}
+    Circle(vec2 position, float radius, float rotation=0.f, Texture* texture=nullptr) :
+        Object(position, radius * 2, radius * 2, rotation, texture), radius(radius) {}
 
-    bool isInside(Vector point) override {
-        return position.distanceTo(point) <= radius;
-    }
+    // bool isInside(vec2 point) override {
+    //     return position.distanceTo(point) <= radius;
+    // }
 
     CollisionInfo getCollisionWith(Circle* other);
     CollisionInfo getCollision(Object& other) override;
  
     virtual void draw() override {
-        drawCircleManual(position, texture, radius, rotation, imageShader);
+        drawCircleManual(position, radius, rotation, texture, imageShader);
     }
 
     virtual void update() override {
@@ -42,55 +42,7 @@ public:
         return "Circle(" + std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(radius) + ")";
     }
 
-    Vector getTopLeft() override {
-        return Vector(position.x - radius, position.y - radius);
-    }
-
-    float getTopLeftX() override {
-        return position.x - radius;
-    }
-
-    float getTopLeftY() override {
-        return position.y - radius;
-    }
-
-    Vector getBottomRight() override {
-        return Vector(position.x + radius, position.y + radius);
-    }
-
-    float getBottomRightX() override {
-        return position.x + radius;
-    }
-
-    float getBottomRightY() override {
-        return position.y + radius;
-    }
-
-    Vector getBottomLeft() override {
-        return Vector(position.x - radius, position.y + radius);
-    }
-
-    float getBottomLeftX() override {
-        return position.x - radius;
-    }
-
-    float getBottomLeftY() override {
-        return position.y + radius;
-    }
-
-    Vector getTopRight() override {
-        return Vector(position.x + radius, position.y - radius);
-    }
-
-    float getTopRightX() override {
-        return position.x + radius;
-    }
-
-    float getTopRightY() override {
-        return position.y - radius;
-    }
-
-    Vector getCenter() override {
+    vec2 getCenter() override {
         return position;
     }
 
@@ -102,59 +54,7 @@ public:
         return position.y;
     }
 
-    void setTopLeft(Vector topLeft) override {
-        position.x = topLeft.x + radius;
-        position.y = topLeft.y + radius;
-    }
-
-    void setTopLeftX(float topLeftX) override {
-        position.x = topLeftX + radius;
-    }
-
-    void setTopLeftY(float topLeftY) override {
-        position.y = topLeftY + radius;
-    }
-
-    void setBottomLeft(Vector bottomLeft) override {
-        position.x = bottomLeft.x + radius;
-        position.y = bottomLeft.y - radius;
-    }
-
-    void setBottomLeftX(float bottomLeftX) override {
-        position.x = bottomLeftX + radius;
-    }
-
-    void setBottomLeftY(float bottomLeftY) override {
-        position.y = bottomLeftY - radius;
-    }
-
-    void setBottomRight(Vector bottomRight) override {
-        position.x = bottomRight.x - radius;
-        position.y = bottomRight.y - radius;
-    }
-
-    void setBottomRightX(float bottomRightX) override {
-        position.x = bottomRightX - radius;
-    }
-
-    void setBottomRightY(float bottomRightY) override {
-        position.y = bottomRightY - radius;
-    }
-
-    void setTopRight(Vector topRight) override {
-        position.x = topRight.x - radius;
-        position.y = topRight.y + radius;
-    }
-
-    void setTopRightX(float topRightX) override {
-        position.x = topRightX - radius;
-    }
-
-    void setTopRightY(float topRightY) override {
-        position.y = topRightY + radius;
-    }
-
-    void setCenter(Vector center) override {
+    void setCenter(vec2 center) override {
         position.x = center.x;
         position.y = center.y;
     }
@@ -170,25 +70,26 @@ public:
 
 class Rectangle : public Object {
 public:
-    Rectangle(int x, int y, int width, int height, Texture* texture=nullptr, float rotation=0.f) :
-        Object(x, y, width, height, texture, rotation) {}
+    Rectangle(float x, float y, float width, float height, float rotation=0.f, Texture* texture=nullptr) :
+        Object(x, y, width, height, rotation, texture) {}
 
-    Rectangle(Vector position, int width, int height, Texture* texture=nullptr, float rotation=0.f) :
-        Object(position, width, height, texture, rotation) {}
+    Rectangle(vec2 position, float width, float height, float rotation=0.f, Texture* texture=nullptr) :
+        Object(position, width, height, rotation, texture) {}
 
-    bool isInside(Vector point) override {
-        return (point.x >= position.x && point.x <= position.x + width &&
-                point.y >= position.y && point.y <= position.y + height);
-    }
+    // bool isInside(vec2 point) override {
+    //     return (point.x >= position.x && point.x <= position.x + width &&
+    //             point.y >= position.y && point.y <= position.y + height);
+    // }
 
-    std::array<glm::vec2, 4> getTransformedVertices();
-    Projection projectVertices(std::array<glm::vec2, 4>, glm::vec2& axis);
+    std::array<vec2, 4> getTransformedVertices();
+    Projection projectVertices(std::array<vec2, 4>, vec2& axis);
     CollisionInfo getCollisionWith(Rectangle* other);
     CollisionInfo getCollisionWith(Circle* other);
     CollisionInfo getCollision(Object& other) override;
 
     virtual void draw() override {
-        drawRectangleManual(position, texture, width, height, imageShader, WHITE, NO_USE_COLOR, rotation);
+        bool useColor = texture->textureID ? NO_USE_COLOR : USE_COLOR;
+        drawRectangleManual(getCenter(), width, height, rotation, texture, imageShader, WHITE, useColor);
     }
 
     virtual void update() override {
@@ -199,56 +100,8 @@ public:
         return "Rectangle(" + std::to_string(position.x) + ", " + std::to_string(position.y) + ", " + std::to_string(width) + ", " + std::to_string(height) + ")";
     }
 
-    Vector getTopLeft() override {
-        return position;
-    }
-
-    float getTopLeftX() override {
-        return position.x;
-    }
-
-    float getTopLeftY() override {
-        return position.y;
-    }
-
-    Vector getBottomRight() override {
-        return Vector(position.x + width, position.y + height);
-    }
-
-    float getBottomRightX() override {
-        return position.x + width;
-    }
-
-    float getBottomRightY() override {
-        return position.y + height;
-    }
-
-    Vector getBottomLeft() override {
-        return Vector(position.x, position.y + height);
-    }
-
-    float getBottomLeftX() override {
-        return position.x;
-    }
-
-    float getBottomLeftY() override {
-        return position.y + height;
-    }
-
-    Vector getTopRight() override {
-        return Vector(position.x + width, position.y);
-    }
-
-    float getTopRightX() override {
-        return position.x + width;
-    }
-
-    float getTopRightY() override {
-        return position.y;
-    }
-
-    Vector getCenter() override {
-        return Vector(position.x + width / 2, position.y + height / 2);
+    vec2 getCenter() override {
+        return vec2(position.x + width / 2, position.y + height / 2);
     }
 
     float getCenterX() override {
@@ -259,7 +112,7 @@ public:
         return position.y + height / 2;
     }
 
-    void setCenter(Vector center) override {
+    void setCenter(vec2 center) override {
         position.x = center.x - width / 2;
         position.y = center.y - height / 2;
     }
@@ -271,60 +124,6 @@ public:
     void setCenterY(float centerY) override {
         position.y = centerY - height / 2;
     }
-
-    void setTopLeft(Vector topLeft) override {
-        position.x = topLeft.x;
-        position.y = topLeft.y;
-    }
-
-    void setTopLeftX(float topLeftX) override {
-        position.x = topLeftX;
-    }
-
-    void setTopLeftY(float topLeftY) override {
-        position.y = topLeftY;
-    }
-
-    void setBottomLeft(Vector bottomLeft) override {
-        position.x = bottomLeft.x;
-        position.y = bottomLeft.y + height;
-    }
-
-    void setBottomLeftX(float bottomLeftX) override {
-        position.x = bottomLeftX;
-    }
-
-    void setBottomLeftY(float bottomLeftY) override {
-        position.y = bottomLeftY + height;
-    }
-
-    void setBottomRight(Vector bottomRight) override {
-        position.x = bottomRight.x + width;
-        position.y = bottomRight.y + height;
-    }
-
-    void setBottomRightX(float bottomRightX) override {
-        position.x = bottomRightX + width;
-    }
-
-    void setBottomRightY(float bottomRightY) override {
-        position.y = bottomRightY + height;
-    }
-
-    void setTopRight(Vector topRight) override {
-        position.x = topRight.x + width;
-        position.y = topRight.y;
-    }
-
-    void setTopRightX(float topRightX) override {
-        position.x = topRightX + width;
-    }
-
-    void setTopRightY(float topRightY) override {
-        position.y = topRightY;
-    }
 };
-
-;
 
 #endif // SHAPE_HPP
