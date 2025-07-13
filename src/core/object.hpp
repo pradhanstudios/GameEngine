308076b90446f496a7d1dfad388348f2882f2048
pradhan-ritik <pradhanr@mailfence.com>
2025-07-12 19:17:26 -0700

    Removed collision related classes

--- src/core/object.hpp
+++ src/core/object.hpp
@@ -6,13 +6,6 @@
 #include "vector.hpp"
 #include "texture.hpp"
 
-struct CollisionInfo {
-    vec2 normal;
-    vec2 mtv;
-    bool collision;
-    CollisionInfo() : normal(vec2(0.f, 0.f)), mtv(vec2(0.f, 0.f)) {}
-};
-
 class Object {
 public:
     vec2 position;
@@ -33,11 +26,6 @@ public:
         rotation(rotation),
 	    texture(texture) {}
     
-    virtual CollisionInfo getCollision(Object&) {
-        // Placeholder for collision detection logic
-        return CollisionInfo();
-    }
-
     virtual void draw() {
         // Placeholder for drawing logic
     }
@@ -56,58 +44,6 @@ public:
     virtual void setCenter(vec2) {}
     virtual void setCenterX(float) {}
     virtual void setCenterY(float) {}
-};
-
-class Collision {
-public:
-    CollisionInfo info;
-
-    Collision() : info(CollisionInfo()) {}
-    void setCollision(Object& object, Object& other) {
-        info = object.getCollision(other);
-    }
-
-    virtual void applyCollisions(vec2& position, vec2& velocity, vec2& acceleration) {
-        if (!info.collision) {
-            return;
-        } 
-        
-        position += info.mtv;
-        float velocityDotNormal = glm::dot(velocity, info.normal);
-
-        if (velocityDotNormal < 0.f) {
-            vec2 normalVelocityComponent = info.normal * velocityDotNormal;
-            vec2 tangentialVelocityComponent = velocity - normalVelocityComponent;
-            const float bounce = 0.f;
-            normalVelocityComponent = -normalVelocityComponent * bounce; 
-            tangentialVelocityComponent *= (1.f - friction);
-            velocity = normalVelocityComponent + tangentialVelocityComponent;
-        }
-
-        // float accelerationDotNormal = glm::dot(acceleration, info.normal);
-
-        // Later implement more advanced accleration system
-        if (glm::dot(info.normal, vec2(0.0f, 1.0f)) > 0.7f) { // Normal is pointing mostly upwards (like a floor)
-            if (acceleration.y < 0) { 
-                acceleration.y = 0; 
-            }
-
-            // If the object's vertical velocity is very small after collision,
-            if (glm::abs(velocity.y) < 0.1f) { // Threshold for "stopped vertically"
-                velocity.y = 0;
-            }
-        }
-
-        if (glm::dot(info.normal, vec2(0.0f, -1.0f)) > 0.7f) {
-            if (acceleration.y > 0) { // If acceleration is pulling upwards (e.g. anti-gravity)
-                acceleration.y = 0;
-            }
-
-            if (glm::abs(velocity.y) < 0.1f) {
-                velocity.y = 0;
-            }
-        }
-    }
 };
 
 class Moveable {
