2658af21ce29d7b72fd016bd03b85f0e629f0acd
rp <pradhanr@mailfence.com>
2025-07-16 20:08:09 -0700

    Added code for .collision functions

--- src/core/shape.cpp
+++ src/core/shape.cpp
@@ -1,3 +1,19 @@
 #include "shape.hpp"
 
+CollisionInfo Rectangle::collision(Object* other) {
+    if (Rectangle* rect = dynamic_cast<Rectangle*>(other))
+        return colliding(this, rect);
+    if (Circle* circle = dynamic_cast<Circle*>(other))
+        return colliding(this, circle);
 
+    return CollisionInfo();
+}
+
+CollisionInfo Circle::collision(Object* other) {
+    if (Rectangle* rect = dynamic_cast<Rectangle*>(other))
+        return colliding(this, rect);
+    if (Circle* circle = dynamic_cast<Circle*>(other))
+        return colliding(this, circle);
+
+    return CollisionInfo();
+}
