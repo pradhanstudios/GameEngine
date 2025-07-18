18c969d2310ae4ab4410c0d8de186ba8da4c8619
pradhan-ritik <pradhanr@mailfence.com>
2025-07-18 12:38:07 -0700

    Include vao.hpp and remove _drawShape

--- src/core/draw.hpp
+++ src/core/draw.hpp
@@ -1,6 +1,7 @@
 #ifndef DRAW_HPP
 #define DRAW_HPP
 #include "constants.hpp"
+#include "vao.hpp"
 #include "vector.hpp"
 #include "texture.hpp"
 
@@ -8,7 +9,6 @@ class Object;
 class Rectangle;
 class Circle;
 
-void _drawShape(const glm::mat4& model, const glm::mat4& view, const glm::mat4& projection, GLuint VAO, GLsizei vertexCount, GLuint shader, GLenum primitiveType, GLenum textureUnit, bool useElements, GLuint textureID, vec3 color, bool useColor);
 void drawRectangle(Rectangle* rect);
 void drawRectangle(vec2 centerPosition, float width, float height, float rotation, Texture* texture, GLuint shader, vec3 color=color::white, bool useColor=color::noUse);
 void drawCircle(Circle* circle);
