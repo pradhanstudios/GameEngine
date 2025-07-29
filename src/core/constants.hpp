d0ff0112b2482863ffc77fbab3de6342a748bf9d
rp <pradhanr@mailfence.com>
2025-07-29 11:38:13 -0700

    Include GLFW

--- src/core/constants.hpp
+++ src/core/constants.hpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <stdio.h> 
 #include <GL/glew.h>
-#include <GL/glut.h> 
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
